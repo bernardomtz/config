1f1fd9bd2546acbf042468b8f9f41fed810660ef
berno <bernardo.mtz@gmail.com>
2015-05-08 00:24:17 -0400

    add basic workspace config files

---
+++ bash_aliases
@@ -0,0 +1,7 @@
+alias aa="vim ~/.bash_aliases"
+alias al="source ~/.bash_aliases"
+
+
+
+github-config="git config --global user.email 'bernardo.mtz@gmail.com'"
+
---
+++ init.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+mkdir -p ~/Documents/code
+cd ~
+ln -s ./Documents/code
+sudo apt-get install -y git
+sudo apt-get install -y openjdk-7-jre
+sudo apt-get install -y g++
+sudo apt-get install -y ucommon-utils
+sudo apt-get install -y vim
+sudo apt-get install -y ruby-full
+sudo apt-get install -y virtualbox
+sudo apt-get install -y unity-webapps-gmail
+
+
+# Install Chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7 # libraries neede by chorme
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+sudo dpkg -i google-chrome*.deb
+#sudo apt-get install debsums 
+
+cd ~
+ln -s code/config/bash_aliases .bash_aliases
+
+
+
